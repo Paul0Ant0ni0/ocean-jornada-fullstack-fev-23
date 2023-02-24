70f7f7f87268d45c93eab929a77773a29a029791
PAULO ANTONIO <96313008+Paul0Ant0ni0@users.noreply.github.com>
2023-02-24 10:59:04 -0300

    URL do backend atualizada

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -59,7 +59,8 @@ function ReadAll() {
 
   // Realizar requisição para backend obtendo a lista de itens
   async function realizarRequisicao() {
-    const url = "http://localhost:3000/item";
+    //const url = "http://localhost:3000/item";
+    const url = "https://ocean-jornada-fullstack-feve-23.onrender.com/item";
     const response = await fetch(url);
     const data = await response.json();
 
